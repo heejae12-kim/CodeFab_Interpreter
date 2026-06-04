ffe05b8aa7b73ee9cdd085fcaab96758e2f7ac38
zentornoe <andycityfc@gmail.com>
2026-06-04 10:44:35 +0900

    [feature] Implement variable declaration

---
+++ CodeFab_Interpreter/Parser.cpp
@@ -0,0 +1,89 @@
+#include "Parser.h"
+
+Parser::Parser(std::vector<Token> tokens_) :
+    tokens_(std::move(tokens_)) {}
+
+std::vector<StmtPtr> Parser::parse() {
+    std::vector<StmtPtr> stmts;
+    while (!isAtEnd()) stmts.push_back(declaration());
+    return stmts;
+}
+
+StmtPtr Parser::declaration() {
+    if (match({ TokenType::VAR })) return
+        varDeclaration();
+    return statement();
+
+}
+
+StmtPtr Parser::varDeclaration() {
+    Token name = consume(TokenType::IDENTIFIER);
+    consume(TokenType::EQUAL);
+    ExprPtr init = expression();
+    consume(TokenType::SEMICOLON);
+    return std::make_unique<VarStmt>(std::move(name),
+        std::move(init));
+}
+StmtPtr Parser::statement() {
+    return expressionStatement();
+}
+
+StmtPtr Parser::expressionStatement() {
+    ExprPtr expr = expression();
+    consume(TokenType::SEMICOLON);
+    return std::make_unique<ExprStmt>(std::move(expr));
+}
+
+ExprPtr Parser::expression() {
+    return primary();
+}
+
+ExprPtr Parser::primary() {
+    if (check(TokenType::NUMBER)) {
+        Token tok = advance();
+        return std::make_unique<LiteralExpr>(tok.getLiteral());
+    }
+    if (check(TokenType::IDENTIFIER)) {
+        Token tok = advance();
+        return std::make_unique<VariableExpr>(std::move(tok));
+    }
+    return nullptr;
+}
+
+
+bool Parser::check(TokenType t)  {
+    return
+        tokens_[current].getTokenType() == t;
+}
+bool Parser::isAtEnd()  {
+    return
+        tokens_[current].getTokenType() == TokenType::EOF_TOKEN;
+}
+Token& Parser::advance() {
+    if (!isAtEnd()) ++current;
+    return tokens_[current - 1];
+}
+
+Token& Parser::peek() { 
+    return tokens_[current]; 
+}
+Token& Parser::previous() {
+    return tokens_[current -1];
+}
+
+bool Parser::match(std::initializer_list<TokenType>
+    types) {
+    for (auto t : types) {
+        if (check(t)) {
+            advance();
+            return true;
+        }
+    }
+    return false;
+}
+
+Token   Parser::consume(TokenType type)
+{
+    if (check(type)) return advance();
+    return peek();
+}
---
+++ CodeFab_Interpreter/ParserTest.cpp
@@ -0,0 +1,48 @@
+#include "Parser.h"
+#include "gmock/gmock.h"
+
+class MockStmtVisitor : public StmtVisitor {
+public:
+    MOCK_METHOD(void, visitPrintStmt, (PrintStmt&),
+        (override));
+    MOCK_METHOD(void, visitExprStmt, (ExprStmt&),
+        (override));
+    MOCK_METHOD(void, visitVarStmt, (VarStmt&),
+        (override));
+    MOCK_METHOD(void, visitBlockStmt, (BlockStmt&),
+        (override));
+    MOCK_METHOD(void, visitIfStmt, (IfStmt&),
+        (override));
+    MOCK_METHOD(void, visitForStmt, (ForStmt&),
+        (override));
+};
+
+TEST(ParserTest, VarDeclaration) {
+    std::vector<Token> tokens = {
+        Token(TokenType::VAR,        "var", nullptr, 1),
+        Token(TokenType::IDENTIFIER, "a",   nullptr, 1),
+        Token(TokenType::EQUAL,      "=",   nullptr, 1),
+        Token(TokenType::NUMBER,     "10",  10.0,    1),
+        Token(TokenType::SEMICOLON,  ";",   nullptr, 1),
+        Token(TokenType::EOF_TOKEN,  "",    nullptr, 1),
+    };
+
+    Parser parser(tokens);
+    auto stmts = parser.parse();
+
+    MockStmtVisitor mock;
+    EXPECT_CALL(mock, visitVarStmt(testing::_))
+        .WillOnce([](VarStmt& stmt) {
+        EXPECT_EQ(stmt.getName().getLexme(), "a");
+        EXPECT_EQ(stmt.getName().getTokenType(),
+            TokenType::IDENTIFIER);
+
+        auto* lit =
+            dynamic_cast<LiteralExpr*>(stmt.getInitializer().get());
+        ASSERT_NE(lit, nullptr);
+
+        EXPECT_DOUBLE_EQ(std::get<double>(lit->getValue()),10.0);
+            });
+
+    stmts[0]->accept(mock);
+}

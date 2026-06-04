1c1fcc950b3fe47c5968504eb0a0b43612afc832
HEEJAE,KIM <kim60212@naver.com>
2026-06-04 16:59:01 +0900

    [Executor] Modify indent in Environment
    Signed-off-by: HEEJAE,KIM <kim60212@naver.com>

--- CodeFab_Interpreter/Environment.cpp
+++ CodeFab_Interpreter/Environment.cpp
@@ -6,14 +6,26 @@ void Environment::define(const std::string& name, ValuableValue value) {
 
 ValuableValue Environment::get(const Token& name) const {
     auto found = values.find(name.getLexme());
-    if (found != values.end()) return found->second;
-    if (enclosing)          return enclosing->get(name);
+    if (found != values.end()) 
+        return found->second;
+
+    if (enclosing)
+        return enclosing->get(name);
+
     throw RuntimeError(name, "Undefined variable '" + name.getLexme() + "'.");
 }
 
 void Environment::assign(const Token& name, ValuableValue value) {
     auto found = values.find(name.getLexme());
-    if (found != values.end()) { found->second = std::move(value); return; }
-    if (enclosing)          { enclosing->assign(name, std::move(value)); return; }
+    if (found != values.end()) { 
+        found->second = std::move(value); 
+        return;
+    }
+
+    if (enclosing) {
+        enclosing->assign(name, std::move(value));
+        return;
+    }
+
     throw RuntimeError(name, "Undefined variable '" + name.getLexme() + "'.");
 }

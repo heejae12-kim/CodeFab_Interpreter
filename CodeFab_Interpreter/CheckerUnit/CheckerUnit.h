c4d7700dc8d7b23af028b12042a58a71525d4f62
darkchsq <darkchsq@naver.com>
2026-06-04 13:10:04 +0900

    [refactor] Change indent rule (before : " " > After : "\t")
    Signed-off-by: darkchsq <darkchsq@naver.com>

--- CodeFab_Interpreter/CheckerUnit/CheckerUnit.h
+++ CodeFab_Interpreter/CheckerUnit/CheckerUnit.h
@@ -17,33 +17,33 @@ public:
 	void doChecker(vector<StmtPtr>& statements_tree_vector);
 
 private:
-    void checkBlcok(const std::vector<StmtPtr>& statements_tree_vector);
-    void checkStatement(Stmt& statements_node);
-    void checkExpression(Expr& expression_node);
+	void checkBlcok(const std::vector<StmtPtr>& statements_tree_vector);
+	void checkStatement(Stmt& statements_node);
+	void checkExpression(Expr& expression_node);
 
 #pragma region ExprVisitor
 
-    ValuableValue visitLiteralExpr(LiteralExpr& expr) override;
-    ValuableValue visitUnaryExpr(UnaryExpr& expr) override;
-    ValuableValue visitBinaryExpr(BinaryExpr& expr) override;
-    ValuableValue visitGroupingExpr(GroupingExpr& expr) override;
-    ValuableValue visitVariableExpr(VariableExpr& expr) override;
-    ValuableValue visitAssignExpr(AssignExpr& expr) override;
+	ValuableValue visitLiteralExpr(LiteralExpr& expr) override;
+	ValuableValue visitUnaryExpr(UnaryExpr& expr) override;
+	ValuableValue visitBinaryExpr(BinaryExpr& expr) override;
+	ValuableValue visitGroupingExpr(GroupingExpr& expr) override;
+	ValuableValue visitVariableExpr(VariableExpr& expr) override;
+	ValuableValue visitAssignExpr(AssignExpr& expr) override;
 
 #pragma endregion
 
 #pragma region StmtVisitor
 
-    void visitPrintStmt(PrintStmt& stmt) override;
-    void visitExprStmt(ExprStmt& stmt) override;
-    void visitVarStmt(VarStmt& stmt) override;
-    void visitBlockStmt(BlockStmt& stmt) override;
-    void visitIfStmt(IfStmt& stmt) override;
-    void visitForStmt(ForStmt& stmt) override;
+	void visitPrintStmt(PrintStmt& stmt) override;
+	void visitExprStmt(ExprStmt& stmt) override;
+	void visitVarStmt(VarStmt& stmt) override;
+	void visitBlockStmt(BlockStmt& stmt) override;
+	void visitIfStmt(IfStmt& stmt) override;
+	void visitForStmt(ForStmt& stmt) override;
 
 #pragma endregion
 
 private:
-    vector<unordered_map<string, bool>> check_values_in_scopes_vector;
+	vector<unordered_map<string, bool>> check_values_in_scopes_vector;
 
 };
--- CodeFab_Interpreter/CheckerUnit/UnitTest/CheckerUnitTest.cpp
+++ CodeFab_Interpreter/CheckerUnit/UnitTest/CheckerUnitTest.cpp
@@ -7,28 +7,28 @@ using namespace testing;
 class CheckerUnitFixture : public Test
 {
 public:
-    void SetUp() override {
-        p_checker_unit = new CheckerUnit();
-    }
+	void SetUp() override {
+		p_checker_unit = new CheckerUnit();
+	}
 
-    void TearDown() override {
-        if (p_checker_unit) delete p_checker_unit;
-        p_checker_unit = nullptr;
-    }
+	void TearDown() override {
+		if (p_checker_unit) delete p_checker_unit;
+		p_checker_unit = nullptr;
+	}
 protected:
-    Token makeIndentifier(const std::string& name, int line = 1) {
-        return Token(TokenType::IDENTIFIER, name, nullptr, line);
-    }
-    ExprPtr numLiteral(double value) {
-        return std::make_unique<LiteralExpr>(value);
-    }
-    StmtPtr valueDeclaration(const std::string& name, ExprPtr init = nullptr, int line = 1) {
-        return std::make_unique<VarStmt>(makeIndentifier(name, line), std::move(init));
-    }
+	Token makeIndentifier(const std::string& name, int line = 1) {
+		return Token(TokenType::IDENTIFIER, name, nullptr, line);
+	}
+	ExprPtr numLiteral(double value) {
+		return std::make_unique<LiteralExpr>(value);
+	}
+	StmtPtr valueDeclaration(const std::string& name, ExprPtr init = nullptr, int line = 1) {
+		return std::make_unique<VarStmt>(makeIndentifier(name, line), std::move(init));
+	}
 
 protected:
-    CheckerUnit* p_checker_unit = nullptr;
-    vector<StmtPtr> statement_vector = {};
+	CheckerUnit* p_checker_unit = nullptr;
+	vector<StmtPtr> statement_vector = {};
 };
 
 TEST_F(CheckerUnitFixture, EmptyProgram) {
@@ -36,22 +36,22 @@ TEST_F(CheckerUnitFixture, EmptyProgram) {
 }
 
 TEST_F(CheckerUnitFixture, DeclareValue) {
-    // var a = 1
-    statement_vector.push_back(valueDeclaration("a", numLiteral(1.0)));
-    EXPECT_NO_THROW(p_checker_unit->doChecker(statement_vector));
+	// var a = 1
+	statement_vector.push_back(valueDeclaration("a", numLiteral(1.0)));
+	EXPECT_NO_THROW(p_checker_unit->doChecker(statement_vector));
 }
 
 TEST_F(CheckerUnitFixture, DeclareDuplicateValueException) {
-    // var a = 10;
-    // var a = 20;
-    statement_vector.push_back(valueDeclaration("a", numLiteral(10.0), 1));
-    statement_vector.push_back(valueDeclaration("a", numLiteral(20.0), 2));
+	// var a = 10;
+	// var a = 20;
+	statement_vector.push_back(valueDeclaration("a", numLiteral(10.0), 1));
+	statement_vector.push_back(valueDeclaration("a", numLiteral(20.0), 2));
 
-    try {
-        p_checker_unit->doChecker(statement_vector);
-        FAIL() << "The CheckerError should occur.";
-    }
-    catch (const CheckerError& e) {
-        EXPECT_THAT(std::string(e.what()), HasSubstr("Already a variable with this name in this scope"));
-    }
+	try {
+		p_checker_unit->doChecker(statement_vector);
+		FAIL() << "The CheckerError should occur.";
+	}
+	catch (const CheckerError& e) {
+		EXPECT_THAT(std::string(e.what()), HasSubstr("Already a variable with this name in this scope"));
+	}
 }
